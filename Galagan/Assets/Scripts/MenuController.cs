201fe81e1da04751eba7343649bf7c87ed177530
Xevion <xevion@xevion.dev>
2024-09-28 12:32:43 -0500

    Setup MenuController

--- Galagan/Assets/Scripts/MenuController.cs
+++ Galagan/Assets/Scripts/MenuController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button _startButton;
+    private Button _quitButton;
+    
+    void Awake()
     {
-        
+        _startButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        _quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _startButton.onClick.AddListener(StartGame);
+        _quitButton.onClick.AddListener(QuitGame);
+    }
+    
+    private static void StartGame()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+    
+    private static void QuitGame()
+    {
+        Application.Quit();
     }
 }
